1c235a1c96f9b963fbe7f7836ef2b240b1372084
aravind2704 <131434602+aravind2704@users.noreply.github.com>
2024-02-17 20:26:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        } else{
+            entry.target.classList.remove('show');
+        }
+
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
+//splash
+
+let intro=document.querySelector('.intro');
+let logo=document.querySelector('.logo-header');
+let logoSpan= document.querySelectorAll('.logo');
+
+window.addEventListener('DOMContentLoaded',()=>{
+
+    setTimeout(() => {
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            },(idx+1)*400)
+        });
+        setTimeout(() => {
+            logoSpan.forEach((span,idx)=>{
+                setTimeout(() => {
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                },(idx+1)*50)
+            })
+        } ,2000)
+
+
+        setTimeout(() => {
+                intro.style.top="-100vh";
+            
+        },2300 );
+
+
+
+
+    } )
+    
+    
+})
